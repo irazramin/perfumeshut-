a716ff70d1b08f77ccf01035a6502b08a2ef6109
irazramin <irazramin@gmail.com>
2022-04-28 23:57:04 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Blogs from "./Components/Blogs/Blogs";
+import ErrorRoute from "./Components/ErrorRoute/ErrorRoute";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Inventoty from "./Components/Inventory/Inventoty";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Header ></Header>
+     <Routes>
+       <Route path="/" element={<Home/>}></Route>
+       <Route path="/inventory" element={<Inventoty/>}></Route>
+       <Route path="/blogs" element={<Blogs/>}></Route>
+       <Route path="*" element={<ErrorRoute/>}></Route>
+     </Routes>
+   </div>
   );
 }
 
---
+++ src/Components/Inventory/Inventoty.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Inventoty = () => {
+  return (
+    <div>Inventoty</div>
+  )
+}
+
+export default Inventoty
