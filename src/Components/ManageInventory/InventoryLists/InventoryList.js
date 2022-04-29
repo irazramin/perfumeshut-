5e398c28c319ea7090af97efef5fc314d053a8ff
irazramin <irazramin@gmail.com>
2022-04-29 18:14:04 +0600

    manage inventory details items added

--- src/Components/ManageInventory/InventoryLists/InventoryList.js
+++ src/Components/ManageInventory/InventoryLists/InventoryList.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const InventoryItem = ({product}) => {
   return (
-   <div>
-       
-   </div>
+    <tr key={product._id}>
+      <td class='px-6 py-4'>{product._id}</td>
+      <td class='px-6 py-4'>{product.perfumsName}</td>
+      <td class='px-6 py-4'>{product.quantity}</td>
+      <td class='px-6 py-4 text-left'>{product.supplier}</td>
+      <td class='px-6 py-4'>${product.price}</td>
+      <td class='px-6 py-4 text-right'></td>
+    </tr>
   );
 }
 
