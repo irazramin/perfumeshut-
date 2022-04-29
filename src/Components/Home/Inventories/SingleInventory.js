aea7cac24b55cf1f1669169206a0a662b9e4d471
irazramin <irazramin@gmail.com>
2022-04-29 12:14:48 +0600

    inventory section updated inventory api fetch

---
+++ src/Components/Home/Inventories/SingleInventory.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const SingleInventory = ({product}) => {
+  const { perfumsName, supplier, quantity, price, description, img } = product;
+  console.log(supplier);
+  return (
+    <div className=''>
+      <div className='grid grid-cols-2 gap-3  p-3 shadow-md'>
+        <div>
+          <img className='w-full h-full object-cover ' src={img} alt='' />
+        </div>
+        <div className=''>
+          <h4 className='text-[22px] font-medium'>{perfumsName}</h4>
+          <h5 className='mt-1 text-xl font-semibold '>${price}</h5>
+          <p className='mt-2 text-[15px]'>
+            {description.length > 30
+              ? description.slice(0, 60) + '....'
+              : description}
+          </p>
+          <span className='mt-2 font-medium  block'>Quantity : {quantity}</span>
+          <div className=' '>
+            <div className='mt-2'>
+              <h5 className='font-medium'>Supplier : {supplier}</h5>
+            </div>
+            <div className='text-right mt-4'>
+              <button className='bg-black text-white px-4 py-2 hover:border-2 hover:border-black hover:bg-white hover:text-black border-2 border-black'>
+                Stoke Update
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleInventory;
---
+++ src/Components/hooks/useFetch.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = () => {
+    const [inventory,setInventory] = useState([]);
+
+    useEffect(() =>{
+        fetch(`https://sheltered-bastion-67111.herokuapp.com/inventory`)
+          .then((res) => res.json())
+          .then((data) => setInventory(data));
+    },[])
+
+    return [inventory];
+}
+
+export default useFetch
